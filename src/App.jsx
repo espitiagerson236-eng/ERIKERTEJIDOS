f546ea358631c82e84fb101bbaff736179787e71
nicol <nicolbel1214@gmail.com>
2026-08-25 09:29:01 -0500

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TopNav } from './components/TopNav';
 import { Header } from './components/Header';
-import { Navbar } from './components/Navbar';
+import { Navbar } from './components/NavBar';
 import { Footer } from './components/Footer';
 import './App.css';
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import logoImg from '../assets/logo.jpeg';
 
 export const Header = () => {
   return (
     <header className="main-header">
       <div className="header-container">
+        <img src={logoImg}alt="logo" className='logo' />
         <a href="/" className="brand-logo">
           ERIKER <span className="highlight">TEJIDOS</span>
         </a>
