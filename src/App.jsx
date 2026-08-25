f0633f1c68137214b280390556a200cac4ba00e8
nicol <nicolbel1214@gmail.com>
2026-08-25 10:40:27 -0500

    whasapyinta

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { TopNav } from './components/TopNav';
 import { Header } from './components/Header';
 import { Navbar } from './components/NavBar';
 import { Footer } from './components/Footer';
+import WhatsAppButton from './components/WhatAppButton';
 import './App.css';
 
 export default function App() {
   return (
     <div className="app-layout">
+      <WhatsAppButton/>
       <TopNav />
       <Header />
       <Navbar />
